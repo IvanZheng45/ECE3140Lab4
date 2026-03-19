a150afbe3636a7206763a27f5fcc940c799a6f88
IvanZheng45 <iz55@cornell.edu>
2026-03-19 19:39:03 -0400

    New Spin lock file

---
+++ lock_spin.c
@@ -0,0 +1,23 @@
+#include "lock.h"
+
+void l_init(lock_t* l) {
+    l->locked = 0;
+}
+
+void l_lock(lock_t* l) {
+    while (1) {
+        __disable_irq();
+        if (l->locked == 0) {
+            l->locked = 1;
+            __enable_irq();
+            return;
+        }
+        __enable_irq();
+    }
+}
+
+void l_unlock(lock_t* l) {
+    __disable_irq();
+    l->locked = 0;
+    __enable_irq();
+}
