7e7f0d324ecc6425c27aa646f7710a903576cdfe
IvanZheng45 <iz55@cornell.edu>
2026-03-23 00:30:44 -0400

    Oops

--- lock_blocking.c
+++ lock_blocking.c
@@ -1,9 +1,12 @@
 #include "lock.h"
 
+extern volatile int total_blocked_processes;
+
 void l_init(lock_t *l) {
 	l->locked = 0;
 	l->blocked_queue.head = NULL;
 }
+
 
 void l_lock(lock_t *l) {
 	__disable_irq();
@@ -14,6 +17,7 @@ void l_lock(lock_t *l) {
 	}
 	current_process_p->blocked = 1;
 	enqueue(current_process_p, &l->blocked_queue);
+	total_blocked_processes++;
 
 	__enable_irq();
 
@@ -29,6 +33,6 @@ void l_unlock(lock_t *l) {
 		removed->blocked = 0;
 		enqueue(removed, &process_queue);
 	}
-
+ 	total_blocked_processes--;
 	__enable_irq();
 }
